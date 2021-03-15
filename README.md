f6ee89c9e2bd8ba4ddc537fdcd67bc4dc423f719
eduardonk9999 <thiago.novak7@gmail.com>
2021-03-15 20:23:03 -0300

    Add, Projeto final

---
+++ README.md
@@ -0,0 +1,5 @@
+### Bitcoin Converter
+
+<a href="" target="_blank">Link Projeto online</a>
+
+![](https://github.com/eduardonk9999/BitcoinConverter/blob/master/imgs/capa.png "Logo") 
--- js/app.js
+++ js/app.js
@@ -6,36 +6,28 @@
 const button = document.querySelector('button');
 const form = document.querySelector('form');
 const input = document.querySelector('.numberMany');
-
+const symbol = document.querySelector(".symbol");
+const valueBitcoin = document.querySelector('.valu-bitcoin');
 
 
 form.addEventListener('submit', event => {
   event.preventDefault();
 
   const selected = event.target.options.value;
-
-  
-
-
+  const valueMany = Number(event.target.many.value);
+  console.log(valueMany)
 
   fetch('https://blockchain.info/ticker')
     .then(response => response.json())
     .then(item => {
-      console.log(item[`${selected}`].last)
+      const valueManyEnd = Number(item[`${selected}`].sell)
+      const calcNumbers = String(valueMany / valueManyEnd)
 
-      
+      const numberFormated = calcNumbers.slice(7, -8)
 
-     
-    // console.log(JSON.stringify(item))
-
-    // const t = item;
-    // console.log(t)
-    // const teste = t + '.' + 'USD'
-    // console.log(teste)
-
-    //  const { last } = 
-      
-    //  console.log(last)
+      symbol.innerHTML = `${item[`${selected}`].symbol}`
+      valueBitcoin.innerHTML = numberFormated
+      console.log(item)
     })
 })
 
